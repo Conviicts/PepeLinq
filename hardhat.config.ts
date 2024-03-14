f9aa60177933570137396d0718fcac5a16f9fe5b
jode-vri <jode-vri@student.42.fr>
2024-03-14 18:28:21 +0100

    Add sepolia network configuration and remove owner parameter in deploy.ts

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,8 +1,15 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
+const secret = require("./secret");
 
 const config: HardhatUserConfig = {
   solidity: "0.8.24",
+  networks: {
+	sepolia: {
+		url: `https://sepolia.infura.io/v3/00e69497300347a38e75c3287621cb16`,
+		accounts: [secret.MMENOMIC],
+	}
+  }
 };
 
 export default config;
--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -1,9 +1,8 @@
 import { ethers } from "hardhat";
 
 async function main() {
-	const owner = "0x8626f6940E2eb28930eFb4CeF49B2d1F2C9C1199";
 
-	const contract = await ethers.deployContract("PepeLinq", [owner]);
+	const contract = await ethers.deployContract("PepeLinq");
 
 	await contract.waitForDeployment();
 
